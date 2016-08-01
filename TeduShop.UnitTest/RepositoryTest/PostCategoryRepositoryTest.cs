d0011f05bec854b34781654243717656693ba3f8
Khanh Xuan Le <khanhlx.56@gmail.com>
2016-08-02 00:38:26 +0700

    Commit bai 10 ver01

---
+++ TeduShop.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+
+namespace TeduShop.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        private IDbFactory dbFactory;
+        private IUnitOfWork unitOfWork;
+        private IPostCategoryRepository objRepository;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            objRepository = new PostCategoryRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_GetAll()
+        {
+            var list = objRepository.GetAll().ToList();
+            
+            Assert.AreEqual(4, list.Count);
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_Create()
+        {
+            PostCategory category = new PostCategory();
+
+            category.Name = "Test category";
+            category.Alias = "Test-category";
+            category.Status = true;
+
+            var result = objRepository.Add(category);
+            unitOfWork.Commit();
+
+            Assert.AreEqual(1, result.ID);
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ TeduShop.UnitTest/ServiceTest/PostCategoryServiceTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.Components.DictionaryAdapter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+using TeduShop.Service;
+
+namespace TeduShop.UnitTest.ServiceTest
+{
+    [TestClass]
+    public class PostCategoryServiceTest
+    {
+        private Mock<IPostCategoryRepository> repositoryMock;
+        private Mock<IUnitOfWork> unitOfWorkMock;
+        private IPostCategoryService categoryService;
+        private List<PostCategory> listCategories;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            repositoryMock = new Mock<IPostCategoryRepository>();
+            unitOfWorkMock = new Mock<IUnitOfWork>();
+            categoryService = new PostCategoryService(repositoryMock.Object, unitOfWorkMock.Object);
+            listCategories = new EditableList<PostCategory>
+            {
+                new PostCategory { ID = 1, Name = "DM1", Status = true},
+                new PostCategory { ID = 2, Name = "DM2", Status = true},
+                new PostCategory { ID = 3, Name = "DM3", Status = true}
+            };
+        }
+
+        [TestMethod]
+        public void PostCategory_Service_GetAll()
+        {
+            // setup method
+            repositoryMock.Setup(m => m.GetAll(null)).Returns(listCategories);
+
+            // call action
+            var result = categoryService.GetAll() as List<PostCategory>;
+
+            // compare
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Count);
+        }
+
+        [TestMethod]
+        public void PostCategory_Service_Create()
+        {
+            PostCategory category = new PostCategory();
+
+            category.Name = "Test";
+            category.Alias = "Test";
+            category.Status = true;
+
+            repositoryMock.Setup(m => m.Add(category)).Returns((PostCategory p) =>
+            {
+                p.ID = 1;
+                return p;
+            });
+
+            //var result = categoryService.Add(category);
+            //Assert.IsNotNull(result);
+            //Assert.AreEqual(1, result.ID);
+        }
+    }
+}

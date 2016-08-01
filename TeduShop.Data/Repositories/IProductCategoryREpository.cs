cb842bfac606249f2ebbe832d56642beab07364c
Khanh Xuan Le <khanhlx.56@gmail.com>
2016-08-01 07:54:33 +0700

    Commit bai 7 ver 1

---
+++ TeduShop.Data/Repositories/IProductCategoryREpository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IProductCategoryREpository
+    {
+        IEnumerable<ProductCategory> GetAsAlias(string alias);
+    }
+}
---
+++ TeduShop.Data/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public class ProductCategoryRepository : RepositoryBase<ProductCategory>, IProductCategoryREpository
+    {
+        public ProductCategoryRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+
+        public IEnumerable<ProductCategory> GetAsAlias(string alias)
+        {
+            return this.DbContext.ProductCategories.Where(x => x.Alias == alias);
+        }
+    }
+}
